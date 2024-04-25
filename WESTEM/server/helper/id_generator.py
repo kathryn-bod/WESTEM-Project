6094a77fc637e5227e2a70b6d297c0b2beff2978
kathryn-bod <144860417+kathryn-bod@users.noreply.github.com>
2024-04-25 15:25:03 -0400

    moved resoure id generator to helper folder

--- WESTEM/server/helper/id_generator.py
+++ WESTEM/server/helper/id_generator.py
@@ -20,3 +20,12 @@ def employee_id(cursor):
         cursor.execute("SELECT * FROM employee WHERE employer_id = %s", (employee_id,))
         if not cursor.fetchone():  # Check if employee ID doesn't already exist
             return employee_id
+        
+def generate_unique_resource_id(cursor):
+    while True:
+        resource_id = random.randint(100000, 999999)
+        cursor.execute("SELECT COUNT(*) FROM resources WHERE resource_id = %s", (resource_id,))
+        count = cursor.fetchone()[0]
+        if count == 0:
+            # Unique resource_id found
+            return resource_id
